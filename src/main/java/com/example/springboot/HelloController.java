36bc67e8756ee872d1c69ce602d7b1c8c18ebca8
Mushtaq Ahmed <30720533+ahmedmq@users.noreply.github.com>
2022-04-07 10:50:23 +0530

    Update For demo

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu Singapore!";
+		return "Greetings from Spring Boot + Tanzu Singapore and India!";
 	}
 
 }
